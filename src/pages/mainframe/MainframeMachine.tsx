358f4ef60b0da614287e4d25a72db2468ceffabb
Dmitry Mikhaylov <business.mikhaylov@gmail.com>
2026-03-13 20:40:29 +0900

    chore: install d3-geo and types

--- src/pages/mainframe/MainframeMachine.tsx
+++ src/pages/mainframe/MainframeMachine.tsx
@@ -1,12 +1,8 @@
-import { useEffect } from 'react'
 import type { JSX } from 'react'
 
 import { OsShell } from '@/widgets/os-shell'
 
 export function MainframeMachine(): JSX.Element {
-  useEffect(() => {
-    void Promise.all([import('d3'), import('d3-geo'), import('topojson-client')])
-  }, [])
 
   return (
     <div className="fixed inset-0 font-mono overflow-hidden" style={{ background: '#0d0b08', color: '#c8a84b' }}>
