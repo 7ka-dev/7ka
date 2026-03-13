471e7acd55293e1efa94dc6d9580093108f895ee
Dmitry Mikhaylov <business.mikhaylov@gmail.com>
2026-03-14 02:14:15 +0900

    chore: updates config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
-import path from "path";
+import path from 'path'
 
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/7ka/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@/app": path.resolve(__dirname, "src/app"),
-      "@/pages": path.resolve(__dirname, "src/pages"),
-      "@/widgets": path.resolve(__dirname, "src/widgets"),
-      "@/features": path.resolve(__dirname, "src/features"),
-      "@/entities": path.resolve(__dirname, "src/entities"),
-      "@/shared": path.resolve(__dirname, "src/shared"),
+      '@/app': path.resolve(__dirname, 'src/app'),
+      '@/pages': path.resolve(__dirname, 'src/pages'),
+      '@/widgets': path.resolve(__dirname, 'src/widgets'),
+      '@/features': path.resolve(__dirname, 'src/features'),
+      '@/entities': path.resolve(__dirname, 'src/entities'),
+      '@/shared': path.resolve(__dirname, 'src/shared'),
     },
   },
-});
+})
